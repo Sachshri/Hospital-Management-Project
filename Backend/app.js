60b73078242d05e0abaa2da7f8945c5174392c20
Sachshri <sk4155765@gmail.com>
2024-09-23 07:56:08 +0530

    Changes Cors Origin

--- Backend/app.js
+++ Backend/app.js
@@ -13,10 +13,10 @@ const app = express();
 config({ path: './config/config.env' });
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL, process.env.DASHBOARD_URL2, process.env.DASHBOARD_URL1],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // This is crucial to allow credentials
+    credentials: true, 
     exposedHeaders: ['Set-Cookie'], 
 }));
 
